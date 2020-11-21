144d7b9e729203643567535b9fd9701555557474
Kenny <kenneththelarson@gmail.com>
2020-11-21 15:59:14 -0700

    first question displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ var startBtn = document.querySelector("#startButton");
 var landingPage = document.querySelector("#title-section");
 var questionsPage = document.querySelector("#quiz-section");
 var questionEl = document.querySelector("#question-title");
+var choicesEl = document.querySelector("#choices");
+
+var currentQuestionIndex = 0;
 
 var questions = [
     {
@@ -10,6 +13,11 @@ var questions = [
         choices: ["pants", "more pants", "a plethora of pants"],
         answer: "a plethora of pants"
     },
+    {
+        title: "Preston is my brother.",
+        choices: ["Yes", "No", "sometimes", "Never"],
+        answer: "Yes"
+    }
 ]
 
 function quizStart() {
@@ -18,7 +26,7 @@ function quizStart() {
 
     var timeLeft = 75;
 
-    var timeInterval = setInterval(function() {
+    var timeInterval = setInterval(function () {
         if (timeLeft > 0) {
             timerEl.textContent = timeLeft;
             timeLeft--;
@@ -32,13 +40,28 @@ function quizStart() {
     questionFunction();
 }
 
-var questionFunction = function() {
-    for (var i = 0; i < questions.length; i++) {
-        var selectedQuestion = questions[i];
+var questionFunction = function () {
+        var selectedQuestion = questions[currentQuestionIndex];
 
         questionEl.textContent = selectedQuestion.title;
-        
-    }
+
+        choicesEl.innerHTML = "";
+
+        selectedQuestion.choices.forEach(function (choice, i) {
+            var choiceButton = document.createElement("button");
+            choiceButton.setAttribute("class", "answer-options");
+            choiceButton.setAttribute("value", choice);
+
+            choiceButton.textContent = i + 1 + ". " + choice;
+
+            choiceButton.onclick = questionClick();
+
+            questionEl.appendChild(choiceButton);
+        });
+}
+
+var questionClick = function() {
+
 }
 
 startBtn.onclick = quizStart;
